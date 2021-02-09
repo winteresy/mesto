cccf3d40c65b217519bca7007e8badb1d73fc7f5
winteresy <alisagurova2@gmail.com>
2021-02-09 15:40:15 +0300

    master

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,44 +1,32 @@
-let popup = document.querySelector('.popup');
-let buttonEdit = document.querySelector('.profile__edit-button');
-let buttonClose = popup.querySelector('.popup__close-icon');
+const popup = document.querySelector('.popup');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonClose = popup.querySelector('.popup__close-icon');
+const buttonSave = document.querySelector('.popup__save');
+const formElement = document.querySelector('.popup__input');
+const profileName = document.querySelector('.profile__name');
+const profileInfo = document.querySelector('.profile__caption');
+const nameInput = document.querySelector('.popup__field_name');
+const jobInput = document.querySelector('.popup__field_profession');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent
+  jobInput.value = profileInfo.textContent
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
-
-const formElement = document.querySelector('.popup__input');
-let profileName = document.querySelector('.profile__name');
-let profileInfo = document.querySelector('.profile__caption');
-let nameInput = document.querySelector('.popup__field_name');
-let jobInput = document.querySelector('.popup__field_profession');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    addPlaceholder();
+    profileName.textContent = nameInput.value
+    profileInfo.textContent = jobInput.value
     popup.classList.remove('popup_opened');
 }
-function addPlaceholder() {
-  profileName.textContent = nameInput.value
-  profileInfo.textContent = jobInput.value
-}
 
+
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
-let likeActive = document.querySelector('.element__like');
-
-function like() {
-  likeActive.classList.add('element__like_active');
-}
-
-function disLike() {
-  likeActive.classList.remove('element__like_active');
-}
-
-likeActive.addEventListener('click', like);
+buttonSave.addEventListener('click', closePopup);
