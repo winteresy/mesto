368ae9104281c706eb2220c659c19a6748b7c39d
winteresy <alisagurova2@gmail.com>
2021-02-09 15:54:59 +0300

    master

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,12 @@
 const popup = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = popup.querySelector('.popup__close-icon');
-const buttonSave = document.querySelector('.popup__save');
+const buttonSave = document.querySelector('.popup__submit-button');
 const formElement = document.querySelector('.popup__input');
 const profileName = document.querySelector('.profile__name');
 const profileInfo = document.querySelector('.profile__caption');
-const nameInput = document.querySelector('.popup__field_name');
-const jobInput = document.querySelector('.popup__field_profession');
+const nameInput = document.querySelector('.popup__field_type_name');
+const jobInput = document.querySelector('.popup__field_type_profession');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -22,7 +22,6 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value
     profileInfo.textContent = jobInput.value
-    popup.classList.remove('popup_opened');
 }
 
 
